4fe037c56a8c6cb4ef59a6a11afdf54422d56c21
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-11-26 10:06:07 -0500

    added tables to db in migrations

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -5,6 +5,34 @@ CREATE TABLE IF NOT EXISTS pups (
   dog_name VARCHAR(255),
   dog_age INTEGER,
   dog_breed TEXT VARCHAR(255),
+  dog_image TEXT,
 )
 
 
+CREATE TABLE IF NOT EXISTS owners (
+  id SERIAL PRIMARY KEY,
+  dog_id INT REFERENCES pups(id),
+  owner_name TEXT VARCHAR(255),
+)
+
+CREATE TABLE IF NOT EXISTS vets (
+  id SERIAL PRIMARY KEY,
+  dog_id INT REFERENCES pups(id),
+  vet_name TEXT VARCHAR(255),
+  vet_address VARCHAR(255),
+  vet_phone INT,
+)
+
+CREATE TABLE IF NOT EXISTS medications (
+  id SERIAL PRIMARY KEY,
+  dog_id INT REFERENCES pups(id),
+  medication_name TEXT VARCHAR(255),
+  medication_dose TEXT VARCHAR(255),
+)
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id SERIAL PRIMARY KEY,
+  owner_id REFERENCES owners(id),
+  task_name TEXT VARCHAR(255),
+  task_status TEXT VARCHAR(255)
+  )
