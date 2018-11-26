95077453ff04c007686700c1728c47bfb570695d
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-11-26 14:33:29 -0500

    added model for pup

--- models/pup.js
+++ models/pup.js
@@ -1,5 +1,50 @@
-const db = require('../db/config.js');
+const db = require('../db/config');
 
-cont Pups = {};
+const Pup = {};
 
+Pups.findAll = () => {
+  return db.query(`SELECT * FROM pups ORDER by id ASC`)
+};
 
+Pups.findById = id => {
+  return db.oneOrNone(`SELECT * FROM pups
+    WHERE id = $1`,
+  [id]
+  );
+};
+
+Pups.create = pup => {
+  return db.one(
+    `INSERT INTO pups (dog_name, dog_age, dog_breed, dog_image)
+    VALUES ($1, $2, $3, $4)
+    RETURNING * `,
+    [pups.dog_name, pups.dog_age, pups.dog_breed, pups.dog_image]
+  );
+};
+
+Pups.update = pup => {
+  retudn db.one(
+    `
+    UPDATE pups SET
+      dog_name = $1,
+      dog_age = $2,
+      dog_breed = $3,
+      dog_image = $4
+      WHERE id = $5
+      RETURNING *
+    `,
+    [pups.dog_name, pups.dog_age, pups.dog_breed, pups.dog_image, id]
+  );
+};
+
+Pups.destroy = id => {
+  return db.none (
+    `
+    DELETE FROM pups
+    WHERE id = $1
+    `,
+    [id]
+  );
+};
+
+module.exports = Pup;
