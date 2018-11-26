162d49902e91b2799e6b2fd4213995f9b05a50b4
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-11-26 10:07:24 -0500

    added create db line

--- db/migrations/migration.sql
+++ db/migrations/migration.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE pups;
 \c pups_dev;
 
 CREATE TABLE IF NOT EXISTS pups (
@@ -6,14 +7,13 @@ CREATE TABLE IF NOT EXISTS pups (
   dog_age INTEGER,
   dog_breed TEXT VARCHAR(255),
   dog_image TEXT,
-)
-
+);
 
 CREATE TABLE IF NOT EXISTS owners (
   id SERIAL PRIMARY KEY,
   dog_id INT REFERENCES pups(id),
   owner_name TEXT VARCHAR(255),
-)
+);
 
 CREATE TABLE IF NOT EXISTS vets (
   id SERIAL PRIMARY KEY,
@@ -21,18 +21,18 @@ CREATE TABLE IF NOT EXISTS vets (
   vet_name TEXT VARCHAR(255),
   vet_address VARCHAR(255),
   vet_phone INT,
-)
+);
 
 CREATE TABLE IF NOT EXISTS medications (
   id SERIAL PRIMARY KEY,
   dog_id INT REFERENCES pups(id),
   medication_name TEXT VARCHAR(255),
   medication_dose TEXT VARCHAR(255),
-)
+);
 
 CREATE TABLE IF NOT EXISTS tasks (
   id SERIAL PRIMARY KEY,
   owner_id REFERENCES owners(id),
   task_name TEXT VARCHAR(255),
   task_status TEXT VARCHAR(255)
-  )
+);
