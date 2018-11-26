09cd1d7ddcb0b82121f62497b3cf06b1e63ba0e6
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-11-26 15:55:23 -0500

    added pup routes

--- routes/pup-routes.js
+++ routes/pup-routes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const pupsRouter = express.Router();
+
+const pupsController = require('../controllers/pups-controllers.js');
+
+pupsRouter.get('/', pupsController.index)
+pupsRouter.post('/', pupsController.create);
+pupsRouter.get('/:id', pupsController.show);
+pupsRouter.put('/:id', pupsController.update);
+pupsRouter.delete('/:id', pupsController.destroy)
+
+module.export = pupsRourter
 
