1bf685b4571262592b58810cc9f50bb3130d387b
Samantha Schonefeld <samanthaschonefeld@Samis-MacBook-Pro.local>
2018-11-28 13:32:48 -0500

    updated owner routes

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dog_name: '',
+      password: '',
+      newId: '',
+      fireRedirect: false,
+    }
+  }
+
+  handleInputChange(e){
+
+  }
+
+  handleSubmit(e){
+
+  }
+
+  handleLogin(e){
+
+  }
+
   render(){
     return(
       <div>
-      this is login
+      <div className = 'login_container'>
+        <label>
+        Dog Name
+        <input
+          type = "text"
+          name = "dog_name"
+          value={this.state.dog_name}
+          placeholder = "Dog Name"
+          required
+          onChange = {(e) => this.handleInputChange(e)}
+        />
+        </label>
+        <label>
+        Password
+        <input
+          type = "password"
+          name = "password"
+          value = {this.state.password}
+          required
+          onChange = {(e) => this.handleInputChange(e)}
+        />
+        </label>
+        <button onClick = {(e) => this.handleLogin(e)}>
+        Login
+        </button>
+      </div>
+      {this.state.fireRedirect ? <Redirect push to={`/landing/${this.state.newId}`} /> : ''}
       </div>
       )
+
   }
 }
 
--- routes/owner-routes.js
+++ routes/owner-routes.js
@@ -3,10 +3,10 @@ const ownerRouter = express.Router();
 
 const ownerController = require('../controllers/owner-controllers.js');
 
-ownerRouter.get('/owner', ownerController.index)
-ownerRouter.post('/owner', ownerController.create);
-ownerRouter.get('/owner/:id', ownerController.show);
-ownerRouter.put('/owner/:id', ownerController.update);
-ownerRouter.delete('/owner/:id', ownerController.destroy)
+ownerRouter.get('/', ownerController.index)
+ownerRouter.post('/', ownerController.create);
+ownerRouter.get('/:id', ownerController.show);
+ownerRouter.put('/:id', ownerController.update);
+ownerRouter.delete('/:id', ownerController.destroy)
 
 module.exports = ownerRouter;
