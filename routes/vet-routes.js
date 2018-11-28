a671af5707cf9d598f317354d2b64a65c786e529
Samantha Schonefeld <samanthaschonefeld@Samis-MacBook-Pro.local>
2018-11-28 13:35:15 -0500

    added vet routes

--- routes/vet-routes.js
+++ routes/vet-routes.js
@@ -3,10 +3,10 @@ const vetRouter = express.Router();
 
 const vetController = require('../controllers/vet-controllers.js');
 
-vetRouter.get('/task', vetController.index)
-vetRouter.post('/task', vetController.create);
-vetRouter.get('/task/:id', vetController.show);
-vetRouter.put('/task/:id', vetController.update);
-vetRouter.delete('/task/:id', vetController.destroy)
+vetRouter.get('/', vetController.index)
+vetRouter.post('/', vetController.create);
+vetRouter.get('/:id', vetController.show);
+vetRouter.put('/:id', vetController.update);
+vetRouter.delete('/:id', vetController.destroy)
 
 module.exports = vetRouter;
