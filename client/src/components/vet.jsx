9f5fef7d3f9e404043b477b090c5b8186f3376fd
Samantha Schonefeld <samanthaschonefeld@ool-ad020500.dyn.optonline.net>
2018-11-29 13:59:02 -0500

    added content to vet page

--- client/src/components/vet.jsx
+++ client/src/components/vet.jsx
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Vet extends Component {
+  state = {
+    vet: null,
+    getData: false
+  }
+
+  componentDidMount(){
+    console.log('this is props from dog: ', this.props)
+    axios.get(`/vet/${this.props.match.params.dog_id}`)
+      .then(res => {
+        this.setState({
+          getData: true,
+          vet: res.data.data
+        })
+      }).catch(err => console.log(err))
+  }
+
+
 render(){
   return(
     <div>
-    this is VET
+    <div className = "vet_title">
+    <h1>Your Dog's Medical Information</h1>
+    </div>
+    <div>
+      <ul>
+        <li>{this.state.vet.vet_name}</li>
+        <li>{this.state.vet.vet_address}</li>
+        <li>{this.state.vet.vet_phone}</li>
+      </ul>
+    </div>
+    <Link to ='/medication/:id'> Medications </Link>
     </div>
     )
   }
